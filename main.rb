94368be71d140811f575ff515bad61911bab8301
Jamie Locatis <jlocatis@gmail.com>
2017-02-27 15:25:07 -0600

    Story and groundfloor framework

---
+++ main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'pry'
+require './functions.rb'
+
+enable :sessions
+
+get "/" do
+	erb :index
+end
+
+post "/submit" do
+	storeInformation(params)
+	redirect("/display")
+end
+
+get "/display" do
+	@submissions = displayInformation()
+	erb :display
+end
