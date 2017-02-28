50fadbd3c9db826a057f4705a16c373d6f4bba49
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-02-28 10:40:37 -0600

    minor changes before master reboot

--- functions.rb
+++ functions.rb
@@ -3,7 +3,7 @@ require 'csv'
 def storeInformation(params)
 	time = Time.now
 	time = time.strftime("%a: %b %d: %I:%M:%P")
-	new_submission = time + "," + params[:name] + "," + params[:submission] + "\n"
+	new_submission = time + "," + params[:name] + "," + params[:submission] + params[:s"\n"
 	File.open('./public/submissions.csv', 'a+') do |file|
 		file << new_submission
 	end
