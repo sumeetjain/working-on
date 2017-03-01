6dd5962d85347a90c90f2fa65d95de69e4ee936b
Jamie Locatis <jlocatis@gmail.com>
2017-03-01 12:00:18 -0600

    First commit

--- controller.rb
+++ controller.rb
@@ -8,6 +8,7 @@ end
 # session["login"] creates a cookie that will keep track of the user.  Creates a new submission
 # class object, and adds the value set (params) gotten from 
 
+
 post "/submit" do
 	session["login"] = params["name"]
   	submission = Submission.new
