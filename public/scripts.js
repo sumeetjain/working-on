0c0c73a9ca047f954f0c1b0cd619109b87ab85b7
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-03 13:58:54 -0600

    Feedback on scroll event

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,7 @@
 window.onscroll = function() {
    	if (document.body.scrollTop + document.documentElement.scrollTop > 100) {
+      // TODO Move all of this style into a CSS class, and use JS to
+      // simply add and remove that class as needed.
    		document.getElementsByClassName("topbar")[0].style.position = "fixed"
    		document.getElementsByClassName("topbar")[0].style.top = "0"
    		document.getElementsByClassName("topbar")[0].style.left = "0"
