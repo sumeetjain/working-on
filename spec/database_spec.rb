d4f1647095bd02e65af2e2e8e4455b95be29ecc6
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-03 14:11:17 -0600

    Use classname in RSpec describe

--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -7,7 +7,7 @@ require './models/posts.rb'
 require 'csv'
 require 'pry'
 
-RSpec.describe("database", '#get_items_by_header') do
+RSpec.describe(Database, '#get_items_by_header') do
   it("creates an Array by loading all the unique items of a specific header catagory") do
     # Setup
     database = Database.new
@@ -20,7 +20,7 @@ RSpec.describe("database", '#get_items_by_header') do
   end
  end
 
- RSpec.describe("database", '#posts_today') do
+ RSpec.describe(Database, '#posts_today') do
     it("Loads data, specifically posts from today,from the storage CSV into an Array for use") do
     # Setup
     database = Database.new
