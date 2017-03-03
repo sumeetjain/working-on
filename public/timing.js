264afd2fe7363549c3d1b0393de91db62929f600
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-03 14:05:37 -0600

    Caching the dailyFeed DOM element

--- public/timing.js
+++ public/timing.js
@@ -1,3 +1,5 @@
+window.dailyFeed = document.getElementById("dailyFeed");
+
 window.addEventListener("load", function(){
  	displayPosts();
 
@@ -10,14 +12,13 @@ function displayPosts() {
   	
   xhttp.onload = function() {
     var dailyPosts = JSON.parse(xhttp.responseText);
-    var top = document.getElementById("dailyFeed");
 
-    document.getElementById("dailyFeed").innerHTML = "";
+    window.dailyFeed.innerHTML = "";
     
     for (x = 0; x < dailyPosts.length; x++) {
       var html = "<div class=\"post thinbluebox\"><div class=\"postProfile\"><div class=\"postname\">" + dailyPosts[x][0] + "</div><div class=\"postdate\">" + dailyPosts[x][1] + "</div></div><div class=\"postsubmission\">" + dailyPosts[x][2] + "</div></div></div>";
 
-    	top.insertAdjacentHTML("afterbegin", html);
+    	window.dailyFeed.insertAdjacentHTML("afterbegin", html);
     }
   };
 
