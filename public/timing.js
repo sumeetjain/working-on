0fcb44a976c2cd624d565765e12fc5d778d8b9ba
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-03 14:01:56 -0600

    indentation

--- public/timing.js
+++ public/timing.js
@@ -6,15 +6,20 @@ window.addEventListener("load", function(){
 
 function displayPosts() {
 	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', '/display')
-  	xhttp.onload = function() {
-    	var dailyPosts = JSON.parse(xhttp.responseText);
-    	var top = document.getElementById("dailyFeed");
-    	document.getElementById("dailyFeed").innerHTML = "";
-    	for (x = 0; x < dailyPosts.length; x++) {
-    		var html = "<div class=\"post thinbluebox\"><div class=\"postProfile\"><div class=\"postname\">" + dailyPosts[x][0] + "</div><div class=\"postdate\">" + dailyPosts[x][1] + "</div></div><div class=\"postsubmission\">" + dailyPosts[x][2] + "</div></div></div>";
-    		top.insertAdjacentHTML("afterbegin", html);
-    	}
-  	};
-  	xhttp.send();
+	xhttp.open('GET', '/display');
+  	
+  xhttp.onload = function() {
+    var dailyPosts = JSON.parse(xhttp.responseText);
+    var top = document.getElementById("dailyFeed");
+
+    document.getElementById("dailyFeed").innerHTML = "";
+    
+    for (x = 0; x < dailyPosts.length; x++) {
+      var html = "<div class=\"post thinbluebox\"><div class=\"postProfile\"><div class=\"postname\">" + dailyPosts[x][0] + "</div><div class=\"postdate\">" + dailyPosts[x][1] + "</div></div><div class=\"postsubmission\">" + dailyPosts[x][2] + "</div></div></div>";
+
+    	top.insertAdjacentHTML("afterbegin", html);
+    }
+  };
+
+  xhttp.send();
 }
