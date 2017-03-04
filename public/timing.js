f73153e047fcbb1e3afee82588afaa1ee1b0f2f1
Michael Sefranek <msefran@gmail.com>
2017-03-04 16:19:12 -0600

    sync

--- public/timing.js
+++ public/timing.js
@@ -1,21 +1,13 @@
 window.addEventListener("load", function(){
-<<<<<<< HEAD
- 	displayPosts();
-
-  dailyFeed = document.getElementById("dailyFeed");
-
-
-=======
   window.dailyFeed = document.getElementById("dailyFeed");
   displayPosts();
->>>>>>> 8181b29b69ddaa8a762c7aa0845b73c27a20c8ff
- 	setInterval(displayPosts, 10000);
+  setInterval(displayPosts, 10000);
 });
 
 function displayPosts() {
-	var xhttp = new XMLHttpRequest();
-	xhttp.open('GET', '/display');
-  	
+  var xhttp = new XMLHttpRequest();
+  xhttp.open('GET', '/display');
+    
   xhttp.onload = function() {
     var dailyPosts = JSON.parse(xhttp.responseText);
 
@@ -24,11 +16,7 @@ function displayPosts() {
     for (x = 0; x < dailyPosts.length; x++) {
       var html = "<div class=\"post thinBox\"><div class=\"postProfile\"><div class=\"postname\">" + dailyPosts[x][0] + "</div><div class=\"postdate\">" + dailyPosts[x][1] + "</div></div><div class=\"postsubmission\">" + dailyPosts[x][2] + "</div></div></div>";
 
-<<<<<<< HEAD
-    	dailyFeed.insertAdjacentHTML("afterbegin", html);
-=======
     dailyFeed.insertAdjacentHTML("afterbegin", html);
->>>>>>> 8181b29b69ddaa8a762c7aa0845b73c27a20c8ff
     }
   };
 
