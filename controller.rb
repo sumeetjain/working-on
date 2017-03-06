cf776c130a8eb72a22245d99c0bad8d1a7b52128
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-03-06 15:36:26 -0600

    merged

--- controller.rb
+++ controller.rb
@@ -33,31 +33,16 @@ end
 
 # Front page display, gets all posts for current day and formats correctly.
 get "/display" do
-<<<<<<< HEAD
-
-	@dailyPosts = $database.posts_today
-
-  # TODO Move all DB functionality into a model/service, e.g.:
-  # @dailyPosts = Post.today
-  
-	@dailyPosts.to_json
-=======
 	dailyPosts = $database.all
 	todays_posts = Posts.new({:day=>Time.now.strftime("%D")}).get_requested_posts_by_date(dailyPosts)
 	@return_posts = Post.new(todays_posts).format_post_front_page
 	@return_posts.to_json
->>>>>>> ead174af7c5c9b987feadb2e83f59e8cf2c5301f
 end
 
 # Sends these params into the Posts class to grab the requested posts for display.
 #
 # Sends admin to getinfo page with their selected search params.
 get "/getinfo" do
-<<<<<<< HEAD
-	@info = Posts.new(params)
-	@info = @info.get_posts_by_date
-  erb :getinfo
-=======
 	@names = Submission.names
   	@dates = Submission.dates
 	posts = Posts.new(params)
@@ -65,5 +50,4 @@ get "/getinfo" do
 	posts = posts.get_requested_posts_by_date(names)
   	@info = Post.new(posts).format_post_admin_page
   	erb :getinfo, :layout => :admin_layout
->>>>>>> ead174af7c5c9b987feadb2e83f59e8cf2c5301f
 end
