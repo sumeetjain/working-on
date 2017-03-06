129a2b2834774a534d63f6877292f6fa484e1b45
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-03-06 10:07:19 -0600

    getinfo.erb has no declaratives, rspec tests moved to containing folder

--- models/post.rb
+++ models/post.rb
@@ -1,11 +1,15 @@
 class Post
-  # TODO Capture single-post related methods here.
-  # (Like what's currently in getinfo.erb.)
+
+#splits up each csv row by ,
+#
+#returns an array
   	def splitRow(post)
   		current_row = post.split(",")
   		return current_row
   	end
-
+#targets the section that holds the time in epoc form
+#
+#returns it in a m/d/y Hr:Min:sec am/pm format
  	def getDate(current_row)
 		date = current_row[0].to_i
 		date = Time.at(date).strftime('%x %I:%M:%S%p')
