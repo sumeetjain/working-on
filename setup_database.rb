926ce23424b26782ecc6cbef9092e94a40d2fdf7
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-07 10:48:25 -0600

    Add instructions for setting up DB

---
+++ setup_database.rb
@@ -0,0 +1,27 @@
+# To install Postgres and the Ruby Postgres adapter:
+
+# brew install postgresql
+# gem install pg -- --with-pg-config=/usr/local/bin/pg_config
+
+# -----------------------------------------------------------------------------
+
+# require "pg"
+
+# # When you need to set up the database, just UNCOMMENT the below.
+# # Remember to comment it back out when you're done.
+
+# conn = PG.connect( dbname: 'postgres' )
+# conn.exec("CREATE DATABASE working_on_development")
+# conn.exec("CREATE DATABASE working_on_test")
+
+# conn = PG.connect( dbname: 'working_on_development' )
+
+# # And create tables...
+# conn.exec("CREATE TABLE submissions (
+#   date        VARCHAR(8), 
+#   time        VARCHAR(30), 
+#   interval    VARCHAR(30), 
+#   name        VARCHAR(255), 
+#   stressLevel INTEGER, 
+#   submission  TEXT
+# )")
