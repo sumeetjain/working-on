af5f506e2b186962580cfc36c44785586805cec7
Jamie Locatis <jlocatis@gmail.com>
2017-03-07 16:49:45 -0600

    admin table created in database and populated with generic login

--- setup_admin_database.rb
+++ setup_admin_database.rb
@@ -1,2 +1,8 @@
-require 'pg'
+# require 'pg'
 
+# conn = PG.connect( dbname: 'working_on_development' )
+
+# conn.exec("CREATE TABLE admin (
+#   username    VARCHAR(10), 
+#   password    VARCHAR(10)
+# )")
