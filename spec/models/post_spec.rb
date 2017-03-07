81e7fae8f142e6f63cfdd177f0354880f9e1f154
Michael Sefranek <msefran@gmail.com>
2017-03-07 10:28:40 -0600

    added test, broke 1 other test

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -22,3 +22,18 @@ RSpec.describe(Post, '#format_post_admin_page') do
 	    expect(student).to eq([["Ben","03/04/17 04:59PM", "02:24:33", "1", "What is this dude talking about?\n"]])
 	end
 end
+
+RSpec.describe(Post, '#gets_all_posts_today') do
+	it("goes through the CSV and returns all posts that occured today, with correct format") do
+		#Setup
+		row = "#{Time.now.to_i},02:24:33,Ben,1,What is this dude talking about?\n"
+		$database.add(row)
+
+		#Exercise
+		posts_today = Post.today
+		#Verify
+		expect(posts_today[0][0] == Time.now.strftime("%D")).to be(true)
+		#Teardown
+
+	end
+end
