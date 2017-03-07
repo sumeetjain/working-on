adfa689c5a5e4cbdf3a42ccdc1c58ed699d462ce
Michael Sefranek <msefran@gmail.com>
2017-03-07 14:48:53 -0600

    added table students

--- setup_database.rb
+++ setup_database.rb
@@ -5,7 +5,7 @@
 
 # -----------------------------------------------------------------------------
 
-
+# require "pg"
 
 # # When you need to set up the database, just UNCOMMENT the below.
 # # Remember to comment it back out when you're done.
@@ -16,7 +16,7 @@
 
 # conn = PG.connect( dbname: 'working_on_development' )
 
-# # And create tables...
+# And create tables...
 # conn.exec("CREATE TABLE submissions (
 #   date        VARCHAR(8), 
 #   time        VARCHAR(30), 
@@ -25,3 +25,11 @@
 #   stressLevel INTEGER, 
 #   submission  TEXT
 # )")
+
+# To instantiate the table for students, include line 17, and line 8
+
+# conn.exec("CREATE TABLE students (
+# 	id SERIAL PRIMARY KEY,
+# 	name VARCHAR(128)
+# )")
+
