b9b2fee47ee0e1a40760e987796bff9ecbeee555
Ben Fairbanks <bdfairbanks3@gmail.com>
2017-03-09 15:20:45 -0600

    making delete function

--- spec/models/posts_spec.rb
+++ spec/models/posts_spec.rb
@@ -1,24 +1,18 @@
-RSpec.describe(Posts, '#split_post_strings') do
-	it("takes an array of strings and splits it into an array of arrays") do
-	    # Setup
-	    post = ["This,is,a,string", "This,is,another,string"]	
-	    # Exercise
-	    student = Posts.new({:student=>"student", :day=>"day"}).split_post_strings(post)
-	    # Verify
-	    expect(student).to eq([["This", "is", "a", "string"], ["This", "is", "another", "string"]]
-		)
 
-	end 	
-end
-
-RSpec.describe(Posts, '#get_requested_posts_by_date') do
-	it("takes an array of posts by name and returns an array of posts by date") do
+RSpec.describe(Posts, '#hold_posts') do
+	it("") do
+	    
 	    # Setup
+	    Database.new.erase_row_by_column_value("Ben", "name", "submissions")
 	    row = ["1488668340,02:24:33,Ben,1,What is this dude talking about?"]
+	    
 	    # Exercise
-	    student = Posts.new({:student=>"Ben", :day=>"03/04/17"}).get_requested_posts_by_date(row)
+	    student = Posts.new({:student=>"Ben", :day=>"03/04/17"}).hold_posts
+	    
 	    # Verify
 	    expect(student).to eq([["1488668340", "02:24:33", "Ben", "1", "What is this dude talking about?"]])
 
+	    # Teardown
+	    Database.new.erase_row_by_column_value("Ben", "name", "submissions")
 	end 	
 end
