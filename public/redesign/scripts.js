e72fdea571df59fce7aebb5e5debb13853e5fa8d
Jamie Locatis <jlocatis@gmail.com>
2017-03-15 10:19:20 -0500

    Fixed login modal, stress level selection

--- public/redesign/scripts.js
+++ public/redesign/scripts.js
@@ -1,15 +1,15 @@
 window.addEventListener("load", function() {
-  document.getElementById("bottom_container-submit_button").addEventListener("click", happyWindow);
+  document.getElementById("admin_button").addEventListener("click", happyWindow);
 });
 
 function happyWindow() {
-  document.getElementById("smiley_modal").style.display = "block";
+  document.getElementById("admin_modal").style.display = "block";
   document.getElementById("modal_background").style.display = "block";
   document.getElementById("modal_background").addEventListener("click", closeModal);
   document.getElementById("close").addEventListener("click", closeModal);
 }
 
 function closeModal() {
-  document.getElementById("smiley_modal").style.display = "none";
+  document.getElementById("admin_modal").style.display = "none";
   document.getElementById("modal_background").style.display = "none";
 }
