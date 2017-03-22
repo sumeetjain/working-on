cd0a963a1d187112064fd50a284742da9ae9113d
Sumeet Jain <sumeet@sumeetjain.com>
2017-03-22 10:32:19 -0500

    Fix SQL error

--- setup_database.rb
+++ setup_database.rb
@@ -5,36 +5,36 @@
 
 # -----------------------------------------------------------------------------
 
-# require "pg"
+require "pg"
 
 # When you need to set up the database, just UNCOMMENT the below.
 # Remember to comment it back out when you're done.
 
-# conn = PG.connect( dbname: 'postgres' )
-# conn.exec("CREATE DATABASE working_on_development")
-# conn.exec("CREATE DATABASE working_on_test")
+conn = PG.connect( dbname: 'postgres' )
+conn.exec("CREATE DATABASE working_on_development")
+conn.exec("CREATE DATABASE working_on_test")
 
-# conn = PG.connect( dbname: 'working_on_development' )
+conn = PG.connect( dbname: 'working_on_development' )
 
 # And create tables...
 
-# conn.exec("CREATE TABLE submissions (
-#   date        VARCHAR(8), 
-#   time        VARCHAR(30), 
-#   interval    VARCHAR(30), 
-#   stressLevel INTEGER, 
-#   submission  TEXT,
-#   id          INTEGER
-# )")
+conn.exec("CREATE TABLE submissions (
+  date        VARCHAR(8), 
+  time        VARCHAR(30), 
+  interval    VARCHAR(30), 
+  stressLevel INTEGER, 
+  submission  TEXT,
+  id          INTEGER
+)")
 
-# conn.exec("CREATE TABLE students (
-# 	id SERIAL PRIMARY KEY,
-# 	name VARCHAR(20)
-#   github VARCHAR(20)
-#   password VARCHAR(20)
-# )")
+conn.exec("CREATE TABLE students (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+  github VARCHAR(20),
+  password VARCHAR(20)
+)")
 
-# conn.exec("CREATE TABLE admin (
-#   username    VARCHAR(10), 
-#   password    VARCHAR(20)
-# )")
+conn.exec("CREATE TABLE admin (
+  username    VARCHAR(10), 
+  password    VARCHAR(20)
+)")
