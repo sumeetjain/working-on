e380088046bd986b9430164b5082e1b6bae36797
Jamie Locatis <jlocatis@gmail.com>
2017-04-18 10:24:05 -0500

    Fixed two issues

--- public/timing.js
+++ public/timing.js
@@ -2,6 +2,7 @@ window.addEventListener("load", function(){
   window.dailyFeed = document.getElementById("middle_container");
 
   displayPosts();
+  setInterval(displayPosts, 10000);
 });
 
 //  sends request to server for data, parses array of daily posts to JSON
@@ -20,7 +21,7 @@ function displayPosts() {
       var html = "<div class=\"post_container\"><div class=\"post_container-left\"><img src=\"https://github.com/" + post[3] + ".png\" class=\"profilePhoto\"></div><div class=\"post_container-right\"><div class=\"post_container-header\"><span class=\"post_container-header_name\">" + post[0] + "</span><span class=\"post_container-header_date\">" + post[1] + "</span></div><div class=\"post_container-content\">" + post[2] + "</div></div></div>";
       dailyFeed.insertAdjacentHTML("afterbegin", html);
     }
-    logout_html = "<a href=\"/reset\" id=\"student_logout\">Logout & Reset User</a>";
+    logout_html = "<a href=\"/reset\" id=\"student_logout\">Logout as Current Student</a>";
     dailyFeed.insertAdjacentHTML("beforeend", logout_html);
   }
   xhttp.send();
